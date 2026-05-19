73a6fe35350116d306987fe76a96acbfb0a58e96
ikercorderoo <ikcolu@jviladoms.cat>
2026-05-19 09:28:26 +0200

    Limpieza

--- scratch/delete_user.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-    await prisma.user.deleteMany({ where: { email: 'aitoreitor3000@gmail.com' } });
-    console.log('Usuario borrado con éxito');
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
